282bf9ff4641b75723467258e91c69b52a6f45fe
Hrishikesh-Bhide <bhidehk@mail.uc.edu>
2025-06-20 14:47:41 -0400

    Added Inheritance example templates

---
+++ object_oriented/Inheritance/Animal.java
@@ -0,0 +1,26 @@
+public class Animal {
+    protected String species;
+    protected int age;
+
+    public Animal(){
+        this.species = "default";
+        this.age = 0;
+    }
+
+    public Animal(String species, int age) {
+        this.species = species;
+        this.age = age;
+    }
+
+    public void breathe() {
+        System.out.println(species + " is breathing.");
+    }
+
+    public void speak() {
+        System.out.println(species + " makes a sound.");
+    }
+
+    public void move() {
+        System.out.println(species + " moves forward.");
+    }
+}
---
+++ object_oriented/Inheritance/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.breathe();
+        d.speak();
+        d.move();
+
+    }
+}
