ed919880bda833af06c6fc464ea0c26c58da54e9
Hrishikesh-Bhide <bhidehk@mail.uc.edu>
2026-06-08 14:04:58 -0400

    Added static and dynamic scope resolution examples

---
+++ imperative/scope/dynamicscope.py
@@ -0,0 +1,22 @@
+def outer(choice):
+  a = "outer contents of a"
+  b = "outer contents of b"
+  c = "outer contents of c"
+  def inner1():
+    a = "inner1 contents of a"
+    b = "inner1 contents of b"
+    inner_inner()
+  def inner2():
+    a = "inner2 contents of a"
+    b = "inner2 contents of b"
+    inner_inner()
+  def inner_inner():
+    a = "inner inner contents of a"
+    print(f"{a=}")
+    print(f"{b=}")
+    print(f"{c=}")
+  if choice == 1:
+    inner1()
+  else:
+    inner2
+outer(1)
