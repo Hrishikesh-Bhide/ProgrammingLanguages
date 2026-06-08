f4113728477c87fb56fb519e4b207cd2fbedea4d
Hrishikesh-Bhide <bhidehk@mail.uc.edu>
2026-06-08 14:05:13 -0400

    Added the python storage binding example

---
+++ imperative/storage/storage_binding.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    result = a + b
+    print(f"Inside function - id(result): {id(result)}, result: {result}")
+    return result
+
+x = 0
+print(f"Before function call - id(x): {id(x)}, x: {x}")
+
+x = add(3, 4)
+print(f"After function call - id(x): {id(x)}, x: {x}")
+
+y = x;
+print(f"After function call - id(y): {id(y)}, y: {y}")
+
+y = y + 1;
+print(f"After function call - id(x): {id(x)}, x: {x}")
+
+print(f"After function call - id(y): {id(y)}, y: {y}")
+
+x = [1,2,3]
+y = x
+y[0] = 4
+print(x[0])
